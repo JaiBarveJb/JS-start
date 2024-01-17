311f60610c84210ec2547a8636bc12e4f52174a0
JaiBarveJb <127341627+JaiBarveJb@users.noreply.github.com>
2024-01-17 20:22:47 +0000

    array2

---
+++ 02_basics/02_array.js
@@ -0,0 +1,22 @@
+const marvel_heroes = ["thor", "Ironman", "spiderman"]
+const dc_heroes = ["superman", "flash", "batman"]
+
+// marvel_heroes.push(dc_heroes)
+
+// console.log(marvel_heroes)
+// console.log(marvel_heroes[3][1])
+
+const allHeroes = marvel_heroes.concat(dc_heroes)
+console.log(allHeroes)
+
+const all_heroes = [...marvel_heroes, ...dc_heroes]
+console.log(all_heroes)
+
+
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+const real_another_array = another_array.flat(Infinity)
+
+console.log(real_another_array)
+
+//Array.isArray , Array.from,  Array.of
